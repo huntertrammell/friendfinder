3a5544f28a8211c0b7ce39c75fbbecee489f175c
Hunter Trammell <hunterttrammell@gmail.com>
2018-11-09 02:24:42 -0600

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArray = [
+    {
+        name: "Hunter Trammell",
+        img: "https://images.gr-assets.com/authors/1518466634p5/17486512.jpg",
+        answers: ['1','1','5','4','2','3','3','3','2','1']
+    }
+]
+
+module.exports = friendsArray;
